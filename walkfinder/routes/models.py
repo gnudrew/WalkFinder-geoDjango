397e09e222d20b170af95984d2bbd754047c125a
Andrew Reid <andrew.reid.mn@gmail.com>
2021-05-13 17:45:52 -0500

    add Model routes/Start; add .gitignore __pycache__

--- walkfinder/routes/models.py
+++ walkfinder/routes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Start(models.Model):
+    target_time = models.IntegerField(default=15)
+    lat = models.DecimalField(decimal_places=6, max_digits=9, default=44.787221)
+    lon = models.DecimalField(decimal_places=6, max_digits=9, default=-93.146939)
