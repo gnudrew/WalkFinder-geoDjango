79d54da46f69cadb55d70dd62a1479a9df90ec37
Andrew Reid <andrew.reid.mn@gmail.com>
2021-05-13 19:35:57 -0500

    Start Model: change default loc and register admin

--- walkfinder/routes/models.py
+++ walkfinder/routes/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Start(models.Model):
     target_time = models.IntegerField(default=15)
-    lat = models.DecimalField(decimal_places=6, max_digits=9, default=44.787221)
-    lon = models.DecimalField(decimal_places=6, max_digits=9, default=-93.146939)
+    lat = models.DecimalField(decimal_places=6, max_digits=9, default=44.786761)
+    lon = models.DecimalField(decimal_places=6, max_digits=9, default=-93.145689)
+    # 44.786761, -93.145689 <-- Lebanon Hills path intersection
