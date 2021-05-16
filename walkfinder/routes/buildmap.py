9dade1080ba96b767d76f95af740432c4fb29d95
Andrew Reid <andrew.reid.mn@gmail.com>
2021-05-16 04:38:27 -0500

    propogate session vars for folium map inputs

--- walkfinder/routes/buildmap.py
+++ walkfinder/routes/buildmap.py
@@ -1,5 +1,5 @@
 import folium
-from random import random as r # interval r() -> x in [0,1)
+
 
 def buildmap_start(lat, lon):
     start_loc=(lat,lon)
@@ -13,23 +13,21 @@ def buildmap_start(lat, lon):
 
     return m
 
-def buildmap_route(m, target_time, start_loc):
+def buildmap_route(m, target_time, start_loc, end_loc):
     # add another feature to the map; placeholder for adding future route
     # randomly add a second marker within (lat+/.005, long+/-.01)
-    rand_lat = (start_loc[0]-.005) + .01*r()
-    rand_lon = (start_loc[1]-.01) + .02*r()
-    rand_loc = ( rand_lat, rand_lon  )
+
 
     # set rezzom box for map after adding new marker
     rezoom_box = [(start_loc[0]-.005, start_loc[1]-.01), (start_loc[0]+.005, start_loc[1]+.01)] # bounding box specified by two points: [southwest, northeast]
 
     print('======== buildmap_route() ========')
     print('start_loc is: ',start_loc)
-    print('rand_loc is: ',rand_loc)
+    print('rand_loc is: ',end_loc)
 
     # add marker at rand_loc
     folium.Marker(
-        rand_loc, # hardcode something in for now
+        end_loc, # hardcode something in for now
         icon=folium.Icon(color='blue'),
         tooltip='click here for detail',
         popup='Target time is '+str(target_time)+' min.',
--- walkfinder/routes/views.py
+++ walkfinder/routes/views.py
@@ -2,35 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .buildmap import buildmap_start, buildmap_route
+from random import random as r # interval r() -> x in [0,1)
 
 # Create your views here.
 def home_view(request):
     # return HttpResponse("<h1>Hello World!</h1>")
     # print(request)
     # print(render(request, "home.html"))
-    request.session['fav_color']='red'
-    print('Home_View session: fav_color is:', request.session['fav_color'])
+
+    # request.session['fav_color']='red'
+    # print('Home_View session: fav_color is:', request.session['fav_color'])
+
     return render(request, "home.html", {})
 
 def mapgen_view(request):
     if request.method=='POST':
+        # get form inputs
         lat = request.POST.get('lat')
         lon = request.POST.get('lon')
+        # store to session
+        request.session['lat'] = lat
+        request.session['lon'] = lon
+        # server-side print
         print('======== mapgen_view ========')
-        print((lat,lon))
-
+        print("(lat,lon) = ",str((lat,lon)))
+        print('====== END mapgen_view ======')
+        # build map
         m = buildmap_start(lat, lon)
-
-        print('~~~ BEGIN SESSION TEST ~~~')
-        fav_color = request.session['fav_color']
-        print('fav_color is:',fav_color)
-        print('Setting fav_color to blue...')
-        request.session['fav_color'] = 'blue'
-        if request.session['fav_color'] == 'blue':
-            print('SUCCESS: new fav_color is blue')
-        else:
-            print('FAILURE: new fav_color not blue')
-        print('~~~ END OF SESSION TEST ~~~')
 
         return render(request, "mapgen.html", {'lat':lat, 'lon':lon, 'folium_map':m._repr_html_()})
     
@@ -44,29 +42,51 @@ def routegen_view(request):
         # for now, I'll regenerate it with hard-coded location.
         # Later, I'll probably store either the map object, or the details needed to regenerate it in the database
         # ... like under some "Previous Map" location in memory.
-        m = buildmap_start(44.784878,-93.143651)
+
+        # retrieve session inputs
+        lat = request.session['lat']
+        lon = request.session['lon']
+        # build map
+        m = buildmap_start(lat,lon)
 
         return render(request, "routegen.html", {'folium_map':m._repr_html_()})
 
     if request.method=='POST':
+        # get form input
         target_time = request.POST.get('target_time')
+        # store to session
+        request.session['target_time'] = target_time
+
         # m is the folium map from the previous call of mapgen_view() or routegen_view().
         # regenerate it for now with hard-coded coordinates
         # Later, I'll probably store either the map object, or the details needed to regenerate it in the database
         # ... like under some "Previous Map" location in memory.
 
-        tmp_lat = 44.784878
-        tmp_lon = -93.143651
+        # retrieve map inputs from session
+        lat = float(request.session['lat'])
+        print('LAT is',lat,'with type',type(lat))
+        lon = float(request.session['lon'])
 
-        m = buildmap_start(tmp_lat, tmp_lon)
-        m = buildmap_route(m, target_time, (tmp_lat, tmp_lon))
+        # randomly generate end lat and lon
+        rand_lat = (lat-.01) + .02*r()
+        rand_lon = (lon-.02) + .04*r()
+        # store to session
+        request.session['rand_lat'] = rand_lat
+        request.session['rand_lon'] = rand_lon
+        # build map
+        m = buildmap_start(lat, lon)
+        m = buildmap_route(m, target_time, (lat, lon), (rand_lat, rand_lon))
         return render(request, "routegen.html", {'target_time':target_time, 'folium_map':m._repr_html_() })
 
 def walk_view(request):
-    tmp_lat = 44.784878
-    tmp_lon = -93.143651
+    # retrieve map inputs from session
+    lat = float(request.session['lat'])
+    lon = float(request.session['lon'])
+    rand_lat = float(request.session['rand_lat'])
+    rand_lon = float(request.session['rand_lon'])
+    target_time = int(request.session['target_time'])
 
-    m = buildmap_start(tmp_lat, tmp_lon)
-    m = buildmap_route(m, 10, (tmp_lat, tmp_lon))
+    m = buildmap_start(lat, lon)
+    m = buildmap_route(m, target_time, (lat, lon), (rand_lat, rand_lon))
     
     return render(request, "walk.html", {'folium_map':m._repr_html_() })
