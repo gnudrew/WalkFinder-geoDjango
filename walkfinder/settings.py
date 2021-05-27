f5e42ae038c595c657c7d68bbe2d030e76249759
Andrew Reid <andrew.reid.mn@gmail.com>
2021-05-27 06:52:40 -0500

    print db config variable before and after

--- walkfinder/settings.py
+++ walkfinder/settings.py
@@ -148,8 +148,10 @@ SECURE_SSL_REDIRECT = True
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-print('===== DB config:', db_from_env, '=====')
+print('===== DB config BEFORE:', db_from_env, '=====')
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+print('===== DB config AFTER:', db_from_env, '=====')
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
