b14c73fd2a3edd00da9f3f915d0de5b5c9803ed7
Andrew Reid <andrew.reid.mn@gmail.com>
2021-05-27 06:20:45 -0500

    update loaded db engine config

--- walkfinder/settings.py
+++ walkfinder/settings.py
@@ -148,6 +148,8 @@ SECURE_SSL_REDIRECT = True
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
+print('===== DB config:', db_from_env, '=====')
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
